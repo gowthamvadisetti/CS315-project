1a8e1fecce5c18db14594b3472e20adc7ece33c4
Gowtham prudhvi <v.g.prudhvi38@gmail.com>
2017-04-13 14:21:27 +0530

    add join before implementing

--- LocalStorageDB.js
+++ LocalStorageDB.js
@@ -551,7 +551,33 @@ License:       MIT License (see homepage)
 
 		this.JOIN = function( table1,table2)
 		{
-			
+			if (!Array.prototype.joinWith) {
+    +function () {
+        Array.prototype.joinWith = function(that, by, select, omit) {
+            var together = [], length = 0;
+            if (select) select.map(function(x){select[x] = 1;});
+            function fields(it) {
+                var f = {}, k;
+                for (k in it) {
+                    if (!select) { f[k] = 1; continue; }
+                    if (omit ? !select[k] : select[k]) f[k] = 1;
+                }
+                return f;
+            }
+            function add(it) {
+                var pkey = '.'+it[by], pobj = {};
+                if (!together[pkey]) together[pkey] = pobj,
+                    together[length++] = pobj;
+                pobj = together[pkey];
+                for (var k in fields(it))
+                    pobj[k] = it[k];
+            }
+            this.map(add);
+            that.map(add);
+            return together;
+        }
+    }();
+}
 		}
 
 	 	/**
