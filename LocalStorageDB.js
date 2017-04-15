aaf4952188f04a2d2ac3a2ee86730ba836b0aaa8
krishnakarthik1309 <krishnakarthik1309@gmail.com>
2017-04-15 07:16:38 +0530

    add function for CROSS-PRODUCT

--- LocalStorageDB.js
+++ LocalStorageDB.js
@@ -581,6 +581,20 @@ License:       MIT License (see homepage)
     		return c;
 		};
 
+		this.CROSS = function(table1, table2, Key1, Key2, select)
+		{
+			var m = table1.length, n = table2.length, index = [], c = [];
+
+    		for (var i = 0; i < m; i++) {     // loop through m items
+        		var row1 = table1[i];
+    			for (var j = 0; j < n; j++) {     // loop through n items
+        			var row2 = table2[j];
+        			c.push(select(row1, row2));
+    			}
+    		}
+    		return c;
+		};
+
 	 	/**
 		 * LocalStorageDB.SELECT( table, criteria ) -> array
 		 * Selects rows from a given table based on the supplied criteria
