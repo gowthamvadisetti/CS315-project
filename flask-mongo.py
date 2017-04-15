ac215f59d2aec3bc2aa20b14d7d86c0d1ab1d34f
krishnakarthik1309 <krishnakarthik1309@gmail.com>
2017-04-15 11:52:45 +0530

    add new collection(table) names2 with url /star2

--- flask-mongo.py
+++ flask-mongo.py
@@ -11,19 +11,17 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/users'
 
 mongo = PyMongo(app)
 
-@app.route('/star_drop', methods=['GET'])
-def drop_stars():
-  star = mongo.db.names
-  try:
-    mongo.db.names.drop()
-    return 'dropped successfully'
-  except:
-    return 'error while dropping users.names table' 
-
-
 @app.route('/star', methods=['GET'])
 def get_all_stars():
   star = mongo.db.names
+  output = []
+  for s in star.find():
+    output.append({'name' : s['name'],'email':s['email'],'id':s['id']})
+  return jsonify( output)
+
+@app.route('/star2', methods=['GET'])
+def get_all_stars2():
+  star = mongo.db.names2
   output = []
   for s in star.find():
     output.append({'name' : s['name'],'email':s['email'],'id':s['id']})
@@ -42,6 +40,38 @@ def add_star():
     new_star = star.find_one({'_id': star_id })
     output.append({'name' : new_star['name'], 'email' : new_star['email'], 'id':new_star['id']})
   return jsonify({'result' : output})
+
+@app.route('/star2', methods=['POST'])
+def add_star2():
+  mongo.db.names2.drop()
+  star = mongo.db.names2
+  output = []
+  for s in request.json:
+    name = s['name']
+    email = s['email']
+    user_id = s['id']
+    star_id = star.insert({'name': name, 'email': email, 'id': user_id})
+    new_star = star.find_one({'_id': star_id })
+    output.append({'name' : new_star['name'], 'email' : new_star['email'], 'id':new_star['id']})
+  return jsonify({'result' : output})
+
+@app.route('/star_drop', methods=['GET'])
+def drop_stars():
+  star = mongo.db.names
+  try:
+    star.drop()
+    return 'users.names dropped successfully'
+  except:
+    return 'error while dropping users.names table'
+
+@app.route('/star_drop2', methods=['GET'])
+def drop_stars2():
+  star = mongo.db.names2
+  try:
+    star.drop()
+    return 'users.names2 dropped successfully'
+  except:
+    return 'error while dropping users.names table'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
